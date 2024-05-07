ebef0813b685b35fefe94d1d982d6f7cd5bafe9a
Jose Garcia Vargas <j.garcia05@live.com>
2024-05-07 13:11:49 -0400

    new url chat ssl

--- src/main/java/com/websocket/chatwebsocket/configuration/WebSocketConfiguration.java
+++ src/main/java/com/websocket/chatwebsocket/configuration/WebSocketConfiguration.java
@@ -19,7 +19,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat-socket")
-                .setAllowedOrigins("http://webchat.appasdn.site/")
+                .setAllowedOrigins("https://webchat.appasdn.site/")
                 .withSockJS();
     }
 }
